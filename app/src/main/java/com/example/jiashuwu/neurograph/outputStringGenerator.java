c74ede9e9653b8867505f3ea496655a5bf68038f
Jiashu Wu <jiwu0083@uni.sydney.edu.au>
2018-03-23 21:15:28 +1100

    Added SendDataEmailActivity layout;

---
+++ app/src/main/java/com/example/jiashuwu/neurograph/outputStringGenerator.java
@@ -0,0 +1,12 @@
+package com.example.jiashuwu.neurograph;
+
+/**
+ * Created by Jiashu Wu on 23/03/2018.
+ */
+
+public class outputStringGenerator {
+
+    // TODO
+    // THIS FILE IS USED TO GENERATE OUTPUT STRINGS.
+
+}
