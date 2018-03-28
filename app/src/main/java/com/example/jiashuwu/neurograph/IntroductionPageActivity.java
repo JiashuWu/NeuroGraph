75ba7491656cdeab6062d8bfc6f25d3707fd973a
Jiashu Wu <jiwu0083@uni.sydney.edu.au>
2018-03-28 12:23:08 +1100

    Added new page: IntroductionPageActivity;

---
+++ app/src/main/java/com/example/jiashuwu/neurograph/IntroductionPageActivity.java
@@ -0,0 +1,13 @@
+package com.example.jiashuwu.neurograph;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroductionPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction_page);
+    }
+}
