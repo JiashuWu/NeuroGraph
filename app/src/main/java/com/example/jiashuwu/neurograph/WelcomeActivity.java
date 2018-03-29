edd11cc02bc8921e230d7b06338b988ca1eb1941
Jiashu Wu <jiwu0083@uni.sydney.edu.au>
2018-03-30 08:54:35 +1100

    Get system language code in WelcomeActivity;

--- app/src/main/java/com/example/jiashuwu/neurograph/WelcomeActivity.java
+++ app/src/main/java/com/example/jiashuwu/neurograph/WelcomeActivity.java
@@ -24,11 +24,27 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        language = Locale.getDefault().getLanguage().toString();
+        language = Locale.getDefault().toString();
+        Log.d("language_code", language);
         switch (language.toLowerCase())
         {
-            case "en": language = "English (EN_GB)";break;
-            case "zh": language = "Simplified Chinese (CH_ZN)";break;
+            case "zh_CN": language = "Simplified Chinese (ZH_CN)";break;
+            case "zh_TW": language = "Traditional Chinese (ZH_TW)";break;
+            case "zh_HK": language = "Traditional Chinese (ZH_HK)";break;
+            case "nl_NL": language = "Dutch Netherlands (NL_NL)";break;
+            case "en_AU": language = "English Australia (EN_AU)";break;
+            case "en_CA": language = "English Canada (EN_CA)";break;
+            case "en_NZ": language = "English New Zealand (EN_NZ)";break;
+            case "en_GB": language = "English Great Britain (EN_GB)";break;
+            case "en_US": language = "English United States (EN_US)";break;
+            case "fr_FR": language = "French France (FR_FR)";break;
+            case "de_DE": language = "German Germany (DE_DE)";break;
+            case "it_IT": language = "Italian Italy (IT_IT)";break;
+            case "ja_JP": language = "Japanese Japan (JA_JP)";break;
+            case "pt_PT": language = "Portuguese Portugal (PT_PT)";break;
+            case "ru_RU": language = "Russian Russia (RU_RU)";break;
+            case "es_ES": language = "Spanish Spain (ES_ES)";break;
+            default: language = "English Great Britain (EN_GB)";break;
         }
         Sharing.language = language;
         Log.d("language", Sharing.language);
