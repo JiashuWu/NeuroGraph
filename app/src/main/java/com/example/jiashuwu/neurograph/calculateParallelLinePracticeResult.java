4023b7776afad367c65c2d7e1f3375bb7390aa4f
Jiashu Wu <jiwu0083@uni.sydney.edu.au>
2018-03-30 20:54:22 +1100

    Added a crossing detection algorithm (STILL UNDER TESTING); NOTICE: The parallel and crossing detection mechanism are still under testing

--- app/src/main/java/com/example/jiashuwu/neurograph/calculateParallelLinePracticeResult.java
+++ app/src/main/java/com/example/jiashuwu/neurograph/calculateParallelLinePracticeResult.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import java.util.ArrayList;
 
 import static java.lang.Math.abs;
+import static java.lang.Math.min;
 
 /**
  * Created by Jiashu Wu on 29/03/2018.
@@ -40,78 +41,80 @@ public class calculateParallelLinePracticeResult {
 
 
 
+        boolean judge = true;
         x_list = Sharing.x_list;
         y_list = Sharing.y_list;
-        Log.d("debuggggg", String.valueOf(x_list.get(1)));
-        int i = 0;
-        int position = 0;
-        for (i = 0 ; i < x_list.size() ; i++)
+        if (x_list.size() == 0 || y_list.size() == 0)
         {
-            if (x_list.get(i) != 0)
-            {
-                x_list_line_1.add(x_list.get(i));
-                y_list_line_1.add(y_list.get(i));
+            judge = false;
+        }
+        if (judge) {
+            Log.d("debuggggg", String.valueOf(x_list.get(1)));
+            int i = 0;
+            int position = 0;
+            for (i = 0; i < x_list.size(); i++) {
+                if (x_list.get(i) != 0) {
+                    x_list_line_1.add(x_list.get(i));
+                    y_list_line_1.add(y_list.get(i));
+                } else {
+                    position = i + 1;
+                    break;
+                }
             }
-            else
-            {
-                position = i + 1;
-                break;
+            for (i = position; i < x_list.size(); i++) {
+                if (x_list.get(i) != 0) {
+                    x_list_line_2.add(x_list.get(i));
+                    y_list_line_2.add(y_list.get(i));
+                } else {
+                    break;
+                }
             }
-        }
-        for (i = position ; i < x_list.size() ; i++)
-        {
-            if (x_list.get(i) != 0)
-            {
-                x_list_line_2.add(x_list.get(i));
-                y_list_line_2.add(y_list.get(i));
+            for (i = 1; i < x_list_line_1.size(); i++) {
+                slope_list_line_1.add((float) ((y_list_line_1.get(i) - y_list_line_1.get(0)) / (x_list_line_1.get(i) - x_list_line_1.get(0))));
             }
-            else
-            {
-                break;
+            for (i = 1; i < x_list_line_2.size(); i++) {
+                slope_list_line_2.add((float) ((y_list_line_2.get(i) - y_list_line_2.get(0)) / (x_list_line_2.get(i) - x_list_line_2.get(0))));
             }
-        }
-        for (i = 1 ; i < x_list_line_1.size() ; i++)
-        {
-            slope_list_line_1.add((float)((y_list_line_1.get(i) - y_list_line_1.get(0)) / (x_list_line_1.get(i) - x_list_line_1.get(0))));
-        }
-        for (i = 1 ; i < x_list_line_2.size() ; i++)
-        {
-            slope_list_line_2.add((float)((y_list_line_2.get(i) - y_list_line_2.get(0)) / (x_list_line_2.get(i) - x_list_line_2.get(0))));
-        }
-        Log.d("summary001", String.valueOf(slope_list_line_1.size()));
-        Log.d("summary002", String.valueOf(slope_list_line_2.size()));
-        float count = 1;
-        float sum = 0;
-        for (i = 0 ; i < slope_list_line_1.size() ; i++)
-        {
-            Log.d("SUMMARYYY", String.valueOf(slope_list_line_1.get(i)));
-        }
-        for (i = 0 ; i < slope_list_line_1.size() ; i++)
-        {
-            summary_slope_line_1 = summary_slope_line_1 + count * slope_list_line_1.get(i);
-            sum = sum + count;
-            count = count + (float)1;
-        }
-        Log.d("summary=", String.valueOf(summary_slope_line_1));
-        Log.d("sum", String.valueOf(sum));
-        summary_slope_line_1 = summary_slope_line_1 / sum;
-        count = 1;
-        sum = 0;
-        for (i = 0 ; i < slope_list_line_2.size() ; i++)
-        {
-            summary_slope_line_2 = summary_slope_line_2 + count * slope_list_line_2.get(i);
-            sum = sum + count;
-            count = count + (float)1;
-        }
-        summary_slope_line_2 = summary_slope_line_2 / sum;
+            Log.d("summary001", String.valueOf(slope_list_line_1.size()));
+            Log.d("summary002", String.valueOf(slope_list_line_2.size()));
+            float count = 1;
+            float sum = 0;
+            for (i = 0; i < slope_list_line_1.size(); i++) {
+                Log.d("SUMMARYYY", String.valueOf(slope_list_line_1.get(i)));
+            }
+            for (i = 0; i < slope_list_line_1.size(); i++) {
+                summary_slope_line_1 = summary_slope_line_1 + count * slope_list_line_1.get(i);
+                sum = sum + count;
+                count = count + (float) 1;
+            }
+            Log.d("summary=", String.valueOf(summary_slope_line_1));
+            Log.d("sum", String.valueOf(sum));
+            summary_slope_line_1 = summary_slope_line_1 / sum;
+            count = 1;
+            sum = 0;
+            for (i = 0; i < slope_list_line_2.size(); i++) {
+                summary_slope_line_2 = summary_slope_line_2 + count * slope_list_line_2.get(i);
+                sum = sum + count;
+                count = count + (float) 1;
+            }
+            summary_slope_line_2 = summary_slope_line_2 / sum;
 
-        Log.d("summary01", String.valueOf(summary_slope_line_1));
-        Log.d("summary02", String.valueOf(summary_slope_line_2));
-        final_result = summary_slope_line_1 - summary_slope_line_2;
-        final_result = Math.abs(final_result);
-        Log.d("FINAL_RESULT", String.valueOf(final_result));
+            Log.d("summary01", String.valueOf(summary_slope_line_1));
+            Log.d("summary02", String.valueOf(summary_slope_line_2));
+            final_result = summary_slope_line_1 - summary_slope_line_2;
+            final_result = Math.abs(final_result);
+            Log.d("FINAL_RESULT", String.valueOf(final_result));
+        }
         String result = "";
         String result_description = "";
+        if (!judge)
+        {
+            result = "No drawing detected";
+            result_description = "No drawing detected! ";
+        }
+
+
+
         if (final_result <= 0.09)
         {
             result = "Perfect";
@@ -132,6 +135,7 @@ public class calculateParallelLinePracticeResult {
             result = "Bad";
             result_description = "The two lines you drew are not parallel";
         }
+
         /* TODO
         if ((y_list_line_1.get(0) - y_list_line_1.get(y_list_line_1.size()-1)) * (y_list_line_2.get(0) - y_list_line_2.size()-1) < 0)
         {
@@ -139,6 +143,16 @@ public class calculateParallelLinePracticeResult {
             result_description = "The two lines you drew cross each other";
         }
         */
+
+        // TODO
+        // DECIDE WHETHER TWO LINES ARE CROSSING EACH OTHER
+
+
+        if (Math.min(x_list_line_1.get(0), x_list_line_1.get(x_list_line_1.size() - 1)) <= Math.max(x_list_line_2.get(0), x_list_line_2.get(x_list_line_2.size() - 1)) && Math.min(y_list_line_2.get(0), y_list_line_2.get(y_list_line_2.size() - 1)) <= Math.max(y_list_line_1.get(0), y_list_line_1.get(y_list_line_1.size() - 1)) && Math.min(x_list_line_2.get(0), x_list_line_2.get(x_list_line_2.size() - 1)) <= Math.max(x_list_line_1.get(0), x_list_line_1.get(x_list_line_1.size() - 1)) && Math.min(y_list_line_1.get(0), y_list_line_1.get(y_list_line_1.size() - 1)) <= Math.max(y_list_line_2.get(0), y_list_line_2.get(y_list_line_2.size() - 1)))
+        {
+            result = "Crossing";
+            result_description = "The two lines you drew cross each other";
+        }
         return new String[]{result, result_description};
     }
 
