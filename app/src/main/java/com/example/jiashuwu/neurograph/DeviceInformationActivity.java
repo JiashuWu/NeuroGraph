8c3228c708765299e4fa331bf7036388bea74331
Jiashu Wu <jiwu0083@uni.sydney.edu.au>
2018-04-16 16:19:59 +1000

    Added a new DeviceInformationActivity;

--- app/src/main/java/com/example/jiashuwu/neurograph/DeviceInformationActivity.java
+++ app/src/main/java/com/example/jiashuwu/neurograph/DeviceInformationActivity.java
@@ -1,25 +1,23 @@
 package com.example.jiashuwu.neurograph;
 
+import android.bluetooth.BluetoothClass;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.icu.text.IDNA;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
-import android.view.KeyEvent;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import java.util.Locale;
 
-public class InfoActivity extends AppCompatActivity {
+public class DeviceInformationActivity extends AppCompatActivity {
 
-    private TextView info_activity_textview_1;
-    private int displayWidth;
-    private int displayHeight;
+    private TextView content_textview;
 
     public void init_theme ()
     {
@@ -64,27 +62,25 @@ public class InfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O)
         {
-            TextScaleUtilsLower.scaleTextSize(InfoActivity.this, Sharing.isScale);
+            TextScaleUtilsLower.scaleTextSize(DeviceInformationActivity.this, Sharing.isScale);
         }
         else
         {
-            TextScaleUtils.scaleTextSize(InfoActivity.this, Sharing.isScale);
+            TextScaleUtils.scaleTextSize(DeviceInformationActivity.this, Sharing.isScale);
         }
         init_theme();
         initLocaleLanguage();
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info);
+        setContentView(R.layout.activity_device_information);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
-        //info_activity_textview_1 = (TextView) findViewById(R.id.info_page_textview1);
-
-        /*
         String device_information_title = "Your device information : \n";
 
         int testing_area_height = Sharing.device_height_in_pixels - Sharing.device_navigation_bar_height;
@@ -96,109 +92,22 @@ public class InfoActivity extends AppCompatActivity {
                 + "Device Resolution : " + Sharing.device_height_in_pixels + " X " + Sharing.device_width_in_pixels + "\n"
                 + "Device Testing Area : " + testing_area_height + " X " + Sharing.device_width_in_pixels + "\n"
                 + "Device Navigation Bar Height : " + Sharing.device_navigation_bar_height + "\n";
-        */
 
-        /*
-        Display display = getWindowManager().getDefaultDisplay();
-        displayWidth = display.getWidth();
-        displayHeight = display.getHeight();
-        */
-
-        /*
-        WindowManager windowManager = (WindowManager) getApplication().getSystemService(Context.WINDOW_SERVICE);
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
-        displayWidth = displayMetrics.widthPixels;
-        displayHeight = displayMetrics.heightPixels;
-        */
-
-        //info_activity_textview_1.setText(device_information_title + device_information);
-
+        content_textview = (TextView) findViewById(R.id.device_information_activity_content_textview);
+        content_textview.setText(device_information_title + "\n" + device_information);
 
     }
 
     @Override
-    public boolean onOptionsItemSelected (MenuItem item)
-    {
+    public boolean onOptionsItemSelected (MenuItem item) {
         int id = item.getItemId();
-        if (id == android.R.id.home) {
-            Intent intent = new Intent (InfoActivity.this, SettingPageActivity.class);
+        if (id == android.R.id.home)
+        {
+            Intent intent = new Intent(DeviceInformationActivity.this, InfoActivity.class);
             startActivity(intent);
-            InfoActivity.this.finish();
-        }
-        else if (id == R.id.action_info_device)
-        {
-            Intent intent = new Intent (InfoActivity.this, DeviceInformationActivity.class);
-            startActivity(intent);
-            InfoActivity.this.finish();
-        }
-        else if (id == R.id.action_info_copyright)
-        {
-
-        }
-        else if (id == R.id.action_info_feedback)
-        {
-
+            DeviceInformationActivity.this.finish();
         }
         return true;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_info_page_activity, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event)
-    {
-        if (keyCode == KeyEvent.KEYCODE_BACK)
-        {
-            Intent intent = new Intent (InfoActivity.this, SettingPageActivity.class);
-            startActivity(intent);
-            InfoActivity.this.finish();
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
-    }
-
-    @Override
-    public void onStart ()
-    {
-        super.onStart();
-    }
-
-    @Override
-    public void onRestart ()
-    {
-        super.onRestart();
-    }
-
-    @Override
-    public void onResume ()
-    {
-        super.onResume();
-    }
-
-    @Override
-    public void onPause ()
-    {
-        super.onPause();
-    }
-
-    @Override
-    public void onStop ()
-    {
-        super.onStop();
-    }
-
-    @Override
-    public void onDestroy ()
-    {
-        super.onDestroy();
     }
 
 }
