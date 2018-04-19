60e4c8f03a4d52bf6dd5a53ebe715c69fba04640
Jiashu Wu <jiwu0083@uni.sydney.edu.au>
2018-04-19 21:38:26 +1000

    Updated SharingIntroduction class which holds the introduction texts;

--- app/src/main/java/com/example/jiashuwu/neurograph/SharingIntroduction.java
+++ app/src/main/java/com/example/jiashuwu/neurograph/SharingIntroduction.java
@@ -6,6 +6,37 @@ package com.example.jiashuwu.neurograph;
 
 public class SharingIntroduction {
 
-    public static String introduction_content = "";
+    public static String introduction_content = "Welcome to Neurograph.\n" +
+            "\n" +
+            "This is an mobile phone application which can be used to assist the diagnostics of Alzheimer's Disease. By doing some tests like drawing a spiral or tracing some parallel lines, we will analyse your drawing pattern and using Artificial Intelligence to detect the level of the disease.\n" +
+            "\n" +
+            "Version 1.0 (2018.06)\n" +
+            "\n" +
+            "This version contains:\n" +
+            "\n" +
+            "User can practice drawing parallel lines before they start the tests\n" +
+            "User can do parallel line test, circular motion test, static background test, static corner background test, dynamic blank background test and dynamic seasonal background test.\n" +
+            "User can choose the test background images\n" +
+            "User can choose their preferred painter width\n" +
+            "The interval duration can be chosen for each dynamic test\n" +
+            "Detailed test instruction before each test\n" +
+            "User can register as new user or login as old user using a registration code\n" +
+            "User can switch user after finishing the tests\n" +
+            "User can choose to store data files and clean data before they switch user\n" +
+            "User can view and edit their personal information\n" +
+            "Data list page which contains all the test informations, you can view and delete your test data\n" +
+            "User can send data email\n" +
+            "User can store data file in several different formats\n" +
+            "User can copy the file path after storing the data files\n" +
+            "Notification will notify the user that the storing has been completed\n" +
+            "User can choose 6 different colour themes\n" +
+            "User can choose 11 different languages\n" +
+            "User can choose 2 kinds of font size\n" +
+            "Well designed tutorial which can assist you to use this app\n" +
+            "Introduction page which contains the release information of this app\n" +
+            "Information page and Copyright page which contains some copyright information\n" +
+            "Device page which can show you some basic properties of your device\n" +
+            "User can change the app setting before or after they login\n" +
+            "User can clean the cache of the app\n";
 
 }
