c855916f5456f78c851c0cfd1745dc8bf1f3b5c1
Jiashu Wu <jiwu0083@uni.sydney.edu.au>
2018-04-19 21:25:00 +1000

    Added SharingIntroduction class which holds the introduction texts;

---
+++ app/src/main/java/com/example/jiashuwu/neurograph/SharingIntroduction.java
@@ -0,0 +1,11 @@
+package com.example.jiashuwu.neurograph;
+
+/**
+ * Created by Jiashu Wu on 19/04/2018.
+ */
+
+public class SharingIntroduction {
+
+    public static String introduction_content = "";
+
+}
